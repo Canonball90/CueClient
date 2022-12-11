b5f1b367b10140866400550b24e2541e5ef50397
Canonball90 <nathanripley2017@gmail.com>
2022-12-11 09:43:09 -0800

    Xray stuff

--- src/main/java/cn/origin/cube/module/modules/visual/XRay.java
+++ src/main/java/cn/origin/cube/module/modules/visual/XRay.java
@@ -4,6 +4,7 @@ import cn.origin.cube.module.Category;
 import cn.origin.cube.module.Module;
 import cn.origin.cube.module.ModuleInfo;
 import cn.origin.cube.module.modules.client.ClickGui;
+import cn.origin.cube.settings.BooleanSetting;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 
@@ -13,6 +14,18 @@ import java.util.ArrayList;
 public class XRay extends Module {
     public static ArrayList<Block> xrayBlocks;
     public static XRay INSTANCE;
+
+    BooleanSetting water = registerSetting("Water", true);
+    BooleanSetting lava = registerSetting("Lava", true);
+    BooleanSetting diamond = registerSetting("Diamond", true);
+    BooleanSetting iron = registerSetting("Iron", true);
+    BooleanSetting gold = registerSetting("Gold", true);
+    BooleanSetting coal = registerSetting("Coal", true);
+    BooleanSetting stone = registerSetting("Stone", true);
+    BooleanSetting quartz = registerSetting("Quartz", true);
+    BooleanSetting lapiz = registerSetting("Lapiz", true);
+    BooleanSetting obsidian = registerSetting("Obsidian", true);
+    BooleanSetting redstone = registerSetting("Redstone", true);
 
     public XRay() {
         INSTANCE = this;
@@ -34,17 +47,61 @@ public class XRay extends Module {
     public void initXray() {
         xrayBlocks = new ArrayList<>();
 
-        xrayBlocks.add(Blocks.DIAMOND_ORE);
-        xrayBlocks.add(Blocks.COAL_ORE);
-        xrayBlocks.add(Blocks.GOLD_ORE);
-        xrayBlocks.add(Blocks.IRON_ORE);
-        xrayBlocks.add(Blocks.LAPIS_ORE);
-        xrayBlocks.add(Blocks.REDSTONE_ORE);
-        xrayBlocks.add(Blocks.LIT_REDSTONE_ORE);
-        xrayBlocks.add(Blocks.QUARTZ_ORE);
-        xrayBlocks.add(Blocks.WATER);
-        xrayBlocks.add(Blocks.FLOWING_WATER);
-        xrayBlocks.add(Blocks.LAVA);
-        xrayBlocks.add(Blocks.FLOWING_LAVA);
+        if(diamond.getValue()) {
+            xrayBlocks.add(Blocks.DIAMOND_ORE);
+        }else{
+            xrayBlocks.remove(Blocks.DIAMOND_ORE);
+        }
+        if(coal.getValue()) {
+            xrayBlocks.add(Blocks.COAL_ORE);
+        }else{
+            xrayBlocks.remove(Blocks.COAL_ORE);
+        }
+        if(gold.getValue()) {
+            xrayBlocks.add(Blocks.GOLD_ORE);
+        }else{
+            xrayBlocks.remove(Blocks.GOLD_ORE);
+        }
+        if(iron.getValue()) {
+            xrayBlocks.add(Blocks.IRON_ORE);
+        }else{
+            xrayBlocks.remove(Blocks.IRON_ORE);
+        }
+        if(lapiz.getValue()) {
+            xrayBlocks.add(Blocks.LAPIS_ORE);
+        }else{
+            xrayBlocks.remove(Blocks.LAPIS_ORE);
+        }
+        if(redstone.getValue()) {
+            xrayBlocks.add(Blocks.REDSTONE_ORE);
+            xrayBlocks.add(Blocks.LIT_REDSTONE_ORE);
+        }else{
+            xrayBlocks.remove(Blocks.REDSTONE_ORE);
+            xrayBlocks.remove(Blocks.LIT_REDSTONE_ORE);
+        }
+        if(quartz.getValue()) {
+            xrayBlocks.add(Blocks.QUARTZ_ORE);
+        }else{
+            xrayBlocks.remove(Blocks.QUARTZ_ORE);
+        }
+        if(water.getValue()) {
+            xrayBlocks.add(Blocks.WATER);
+            xrayBlocks.add(Blocks.FLOWING_WATER);
+        }else{
+            xrayBlocks.remove(Blocks.WATER);
+            xrayBlocks.remove(Blocks.FLOWING_WATER);
+        }
+        if(lava.getValue()) {
+            xrayBlocks.add(Blocks.LAVA);
+            xrayBlocks.add(Blocks.FLOWING_LAVA);
+        }else{
+            xrayBlocks.remove(Blocks.LAVA);
+            xrayBlocks.remove(Blocks.FLOWING_LAVA);
+        }
+        if(obsidian.getValue()){
+            xrayBlocks.add(Blocks.OBSIDIAN);
+        }else{
+            xrayBlocks.remove(Blocks.OBSIDIAN);
+        }
     }
 }
