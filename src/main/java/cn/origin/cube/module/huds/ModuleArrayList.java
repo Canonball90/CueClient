bfa7a655e5016dd62f47fe523ed2e382873e2a2c
Canonball90 <nathanripley2017@gmail.com>
2022-12-11 06:44:37 -0800

    Fade stuff

--- src/main/java/cn/origin/cube/module/huds/ModuleArrayList.java
+++ src/main/java/cn/origin/cube/module/huds/ModuleArrayList.java
@@ -5,7 +5,9 @@ import cn.origin.cube.module.*;
 import cn.origin.cube.module.modules.client.ClickGui;
 import cn.origin.cube.settings.BooleanSetting;
 import cn.origin.cube.settings.ModeSetting;
+import net.minecraft.client.Minecraft;
 
+import java.awt.*;
 import java.util.Comparator;
 import java.util.stream.Collectors;
 
@@ -14,7 +16,7 @@ public class ModuleArrayList extends HudModule {
 
     public ModeSetting<?> alignSetting = registerSetting("Align", alignMode.Left);
     public ModeSetting<?> sortSetting = registerSetting("Sort", sortMode.Top);
-    public BooleanSetting colorNew = registerSetting("CoolColor", false);
+    public ModeSetting<?> colorSetting = registerSetting("Color", colorMode.Fade);
     public int count = 0;
 
     @Override
@@ -28,16 +30,38 @@ public class ModuleArrayList extends HudModule {
                 .forEach(module -> {
                     float modWidth = Cube.fontManager.CustomFont.getStringWidth(module.getFullHud());
                     String modText = module.getFullHud();
-                    if (alignMode.Right.equals(alignSetting.getValue())) {
-                        Cube.fontManager.CustomFont.drawStringWithShadow(modText,
-                                (int) (this.x - 2 - modWidth + this.width),
-                                this.y + (10 * count),
-                                (colorNew.getValue()) ?module.category.getColor().getRGB() : ClickGui.getCurrentColor().getRGB());
-                    } else {
-                        Cube.fontManager.CustomFont.drawStringWithShadow(modText,
-                                this.x - 2,
-                                this.y + (10 * count),
-                                (colorNew.getValue()) ?module.category.getColor().getRGB() : ClickGui.getCurrentColor().getRGB());
+                    Color c;
+                    if(colorSetting.getValue().equals(colorMode.CategoryC)){
+                        c = module.category.getColor();
+                    }
+                    if(colorSetting.getValue().equals(colorMode.Fade)){
+                        c = ClickGui.getCurrentColor();
+                    }
+                    else{
+                        c = ClickGui.getCurrentColor();
+                    }
+                    if(colorSetting.getValue().equals(colorMode.Static) || colorSetting.getValue().equals(colorMode.CategoryC)) {
+                        if (alignMode.Right.equals(alignSetting.getValue())) {
+                            Cube.fontManager.CustomFont.drawStringWithShadow(modText,
+                                    (int) (this.x - 2 - modWidth + this.width),
+                                    this.y + (10 * count),
+                                    c.getRGB());
+                        } else {
+                            Cube.fontManager.CustomFont.drawStringWithShadow(modText,
+                                    this.x - 2,
+                                    this.y + (10 * count),
+                                    c.getRGB());
+                        }
+                    }else{
+                        if (alignMode.Right.equals(alignSetting.getValue())) {
+                            drawFadeString(modText,
+                                    (int) (this.x - 2 - modWidth + this.width),
+                                    this.y + (10 * count));
+                        } else {
+                            drawFadeString(modText,
+                                    this.x - 2,
+                                    this.y + (10 * count));
+                        }
                     }
                     count++;
                 });
@@ -54,9 +78,42 @@ public class ModuleArrayList extends HudModule {
                         .filter(AbstractModule::isEnabled).count());
     }
 
+    public static void drawFadeString(final String s, float x, float y) {
+        float updateX = x;
+
+        for (int i = 0; i < s.length(); i++) {
+            String str = String.valueOf(s.charAt(i));
+
+            Minecraft mc = Minecraft.getMinecraft();
+            double colorOffset = (Math.abs(((System.currentTimeMillis()) / 20D)) / 50) + (50 / (mc.fontRenderer.FONT_HEIGHT + i * 14f + 50D));
+            Color color = getGradientOffset1(new Color(0xFFFFFF), new Color(0x0081FF), colorOffset, 190);
+
+            mc.fontRenderer.drawString(str, (int) updateX, (int) y, color.hashCode());
+            updateX += mc.fontRenderer.getStringWidth(str);
+        }
+    }
+
+    public static Color getGradientOffset1(final Color color1, final Color color2, double offset, final int alpha) {
+        if (offset > 1) {
+            double left = offset % 1;
+            int off = (int) offset;
+            offset = off % 2 == 0 ? left : 1 - left;
+
+        }
+        final double inverse_percent = 1 - offset;
+        final int redPart = (int) (color1.getRed() * inverse_percent + color2.getRed() * offset);
+        final int greenPart = (int) (color1.getGreen() * inverse_percent + color2.getGreen() * offset);
+        final int bluePart = (int) (color1.getBlue() * inverse_percent + color2.getBlue() * offset);
+        return new Color(redPart, greenPart, bluePart, alpha);
+    }
+
     enum alignMode {
         Left,
         Right
+    }
+
+    enum colorMode {
+        CategoryC,Fade,Static
     }
 
     enum sortMode {
--- src/main/java/cn/origin/cube/module/huds/WaterMark.java
+++ src/main/java/cn/origin/cube/module/huds/WaterMark.java
@@ -8,7 +8,10 @@ import cn.origin.cube.module.modules.client.ClickGui;
 import cn.origin.cube.settings.BooleanSetting;
 import cn.origin.cube.settings.FloatSetting;
 import cn.origin.cube.settings.ModeSetting;
+import net.minecraft.client.Minecraft;
 import org.lwjgl.opengl.GL11;
+
+import java.awt.*;
 
 @HudModuleInfo(name = "WaterMark", x = 114, y = 114, descriptions = "Show hack name", category = Category.HUD)
 public class WaterMark extends HudModule {
@@ -22,7 +25,7 @@ public class WaterMark extends HudModule {
         GL11.glTranslated(this.x, (float) this.y, 0);
         GL11.glScaled((double) this.Scala.getValue(), (double) this.Scala.getValue(), 0.0);
         if(mode.getValue().equals(nfd.Word)) {
-            Cube.fontManager.CustomFont.drawString("CueClient", 0, 0, ClickGui.getCurrentColor().getRGB());
+            drawFadeString("CueClient", 0, 0);
         }
         if(mode.getValue().equals(nfd.Logo)) {
             Cube.fontManager.IconFont.drawString("t", 0, 0, ClickGui.getCurrentColor().getRGB());
@@ -37,6 +40,38 @@ public class WaterMark extends HudModule {
         GL11.glPopMatrix();
         this.width = (int) ((float) Cube.fontManager.CustomFont.getStringWidth("CueClient") * this.Scala.getValue());
         this.height = (int) ((float) Cube.fontManager.CustomFont.getHeight() * this.Scala.getValue());
+    }
+
+    private void drawFadeString(int scaledWidth, int y, int i, int i1) {
+    }
+
+    public static void drawFadeString(final String s, float x, float y) {
+        float updateX = x;
+
+        for (int i = 0; i < s.length(); i++) {
+            String str = String.valueOf(s.charAt(i));
+
+            Minecraft mc = Minecraft.getMinecraft();
+            double colorOffset = (Math.abs(((System.currentTimeMillis()) / 20D)) / 50) + (50 / (mc.fontRenderer.FONT_HEIGHT + i * 14f + 50D));
+            Color color = getGradientOffset1(new Color(0xFFFFFF), new Color(0x0081FF), colorOffset, 160);
+
+            Cube.fontManager.CustomFont.drawString(str, (int) updateX, (int) y, color.hashCode());
+            updateX += mc.fontRenderer.getStringWidth(str);
+        }
+    }
+
+    public static Color getGradientOffset1(final Color color1, final Color color2, double offset, final int alpha) {
+        if (offset > 1) {
+            double left = offset % 1;
+            int off = (int) offset;
+            offset = off % 2 == 0 ? left : 1 - left;
+
+        }
+        final double inverse_percent = 1 - offset;
+        final int redPart = (int) (color1.getRed() * inverse_percent + color2.getRed() * offset);
+        final int greenPart = (int) (color1.getGreen() * inverse_percent + color2.getGreen() * offset);
+        final int bluePart = (int) (color1.getBlue() * inverse_percent + color2.getBlue() * offset);
+        return new Color(redPart, greenPart, bluePart, alpha);
     }
 
     public enum nfd{
