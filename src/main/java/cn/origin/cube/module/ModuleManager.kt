16b44568d4ab98d73a43923d4c486ceb9e02d0f0
Canonball90 <nathanripley2017@gmail.com>
2022-12-11 07:15:48 -0800

    stuff

--- src/main/java/cn/origin/cube/module/ModuleManager.kt
+++ src/main/java/cn/origin/cube/module/ModuleManager.kt
@@ -24,6 +24,7 @@ class ModuleManager {
         registerModule(Console())
         registerModule(AutoConfig())
         registerModule(MainMenuShader())
+        registerModule(Test())
 
         //Combat
         registerModule(Surround())
