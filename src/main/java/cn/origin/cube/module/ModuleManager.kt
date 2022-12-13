cf062311c44dbb5f762ad0c32ea24f55879bbbd7
Canonball90 <46059283+Canonball90@users.noreply.github.com>
2022-12-12 19:22:14 -0500

    Skeet Gui

--- src/main/java/cn/origin/cube/module/ModuleManager.kt
+++ src/main/java/cn/origin/cube/module/ModuleManager.kt
@@ -24,6 +24,7 @@ class ModuleManager {
         registerModule(Console())
         registerModule(AutoConfig())
         registerModule(MainMenuShader())
+        registerModule(SkeetSkeet())
 
         //Combat
         registerModule(Surround())
