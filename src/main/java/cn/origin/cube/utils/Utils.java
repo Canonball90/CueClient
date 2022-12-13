14d567339f5654e148e19edf534436ec89ba6777
Canonball90 <46059283+Canonball90@users.noreply.github.com>
2022-12-13 00:30:47 -0500

    Changes

--- src/main/java/cn/origin/cube/utils/Utils.java
+++ src/main/java/cn/origin/cube/utils/Utils.java
@@ -3,6 +3,7 @@ package cn.origin.cube.utils;
 import net.minecraft.client.Minecraft;
 
 public class Utils {
+    public static Utils INSTANCE = new Utils();
     public static final Minecraft mc = Minecraft.getMinecraft();
     public static boolean nullCheck() {
         return mc.player == null || mc.world == null;
