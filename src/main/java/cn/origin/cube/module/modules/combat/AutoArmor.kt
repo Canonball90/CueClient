53c6f193b7f2f6d57375e0fc81e3a1a77343494a
Canonball90 <46059283+Canonball90@users.noreply.github.com>
2022-12-19 11:49:04 -0500

    Added delay to autoarmor

--- src/main/java/cn/origin/cube/module/modules/combat/AutoArmor.kt
+++ src/main/java/cn/origin/cube/module/modules/combat/AutoArmor.kt
@@ -1,8 +1,10 @@
 package cn.origin.cube.module.modules.combat
 
+import cn.origin.cube.core.settings.DoubleSetting
 import cn.origin.cube.module.Category
 import cn.origin.cube.module.Module
 import cn.origin.cube.module.interfaces.ModuleInfo
+import cn.origin.cube.utils.Timer
 import net.minecraft.client.gui.inventory.GuiContainer
 import net.minecraft.client.renderer.InventoryEffectRenderer
 import net.minecraft.init.Items
@@ -17,43 +19,49 @@ import net.minecraftforge.fml.common.gameevent.TickEvent
     descriptions = "AutoArmor",
     category = Category.COMBAT)
 class AutoArmor:Module() {
+    var delay: DoubleSetting = registerSetting("Delay", 100.0, 100.0, 1000.0)
+
+    private val timer = Timer()
 
     @SubscribeEvent
     fun onTick(event: TickEvent.ClientTickEvent?) {
         if (!(mc.player == null || mc.world == null)) {
-            if (mc.player.ticksExisted % 2 == 0) return
-            if (mc.currentScreen is GuiContainer && mc.currentScreen !is InventoryEffectRenderer) return
-            val bestArmorSlots = IntArray(4)
-            val bestArmorValues = IntArray(4)
-            for (armorType in 0..3) {
-                val oldArmor = mc.player.inventory.armorItemInSlot(armorType)
-                if (oldArmor.item is ItemArmor) bestArmorValues[armorType] =
-                    (oldArmor.item as ItemArmor).damageReduceAmount
-                bestArmorSlots[armorType] = -1
-            }
-            for (slot in 0..35) {
-                val stack = mc.player.inventory.getStackInSlot(slot)
-                if (stack.count > 1) continue
-                if (stack.item !is ItemArmor) continue
-                val armor = stack.item as ItemArmor
-                val armorType = armor.armorType.ordinal - 2
-                if (armorType == 2 && mc.player.inventory.armorItemInSlot(armorType).item == Items.ELYTRA) continue
-                val armorValue = armor.damageReduceAmount
-                if (armorValue > bestArmorValues[armorType]) {
-                    bestArmorSlots[armorType] = slot
-                    bestArmorValues[armorType] = armorValue
+            if(timer.passed(delay.value.toLong())) {
+                if (mc.player.ticksExisted % 2 == 0) return
+                if (mc.currentScreen is GuiContainer && mc.currentScreen !is InventoryEffectRenderer) return
+                val bestArmorSlots = IntArray(4)
+                val bestArmorValues = IntArray(4)
+                for (armorType in 0..3) {
+                    val oldArmor = mc.player.inventory.armorItemInSlot(armorType)
+                    if (oldArmor.item is ItemArmor) bestArmorValues[armorType] =
+                        (oldArmor.item as ItemArmor).damageReduceAmount
+                    bestArmorSlots[armorType] = -1
                 }
-            }
-            for (armorType in 0..3) {
-                var slot = bestArmorSlots[armorType]
-                if (slot == -1) continue
-                val oldArmor = mc.player.inventory.armorItemInSlot(armorType)
-                if (oldArmor != ItemStack.EMPTY || mc.player.inventory.firstEmptyStack != -1) {
-                    if (slot < 9) slot += 36
-                    mc.playerController.windowClick(0, 8 - armorType, 0, ClickType.QUICK_MOVE, mc.player)
-                    mc.playerController.windowClick(0, slot, 0, ClickType.QUICK_MOVE, mc.player)
-                    break
+                for (slot in 0..35) {
+                    val stack = mc.player.inventory.getStackInSlot(slot)
+                    if (stack.count > 1) continue
+                    if (stack.item !is ItemArmor) continue
+                    val armor = stack.item as ItemArmor
+                    val armorType = armor.armorType.ordinal - 2
+                    if (armorType == 2 && mc.player.inventory.armorItemInSlot(armorType).item == Items.ELYTRA) continue
+                    val armorValue = armor.damageReduceAmount
+                    if (armorValue > bestArmorValues[armorType]) {
+                        bestArmorSlots[armorType] = slot
+                        bestArmorValues[armorType] = armorValue
+                    }
                 }
+                for (armorType in 0..3) {
+                    var slot = bestArmorSlots[armorType]
+                    if (slot == -1) continue
+                    val oldArmor = mc.player.inventory.armorItemInSlot(armorType)
+                    if (oldArmor != ItemStack.EMPTY || mc.player.inventory.firstEmptyStack != -1) {
+                        if (slot < 9) slot += 36
+                        mc.playerController.windowClick(0, 8 - armorType, 0, ClickType.QUICK_MOVE, mc.player)
+                        mc.playerController.windowClick(0, slot, 0, ClickType.QUICK_MOVE, mc.player)
+                        break
+                    }
+                }
+                timer.reset()
             }
         }
     }
