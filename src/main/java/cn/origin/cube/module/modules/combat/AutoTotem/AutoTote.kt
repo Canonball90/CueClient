b1e2052bf9652deb92b63ecbdd822bc7f14273b0
Canonball90 <46059283+Canonball90@users.noreply.github.com>
2022-12-25 12:50:29 -0500

    AT Changes

--- src/main/java/cn/origin/cube/module/modules/combat/AutoTotem/AutoTote.kt
+++ src/main/java/cn/origin/cube/module/modules/combat/AutoTotem/AutoTote.kt
@@ -5,7 +5,9 @@ import cn.origin.cube.core.settings.BooleanSetting
 import cn.origin.cube.core.settings.FloatSetting
 import cn.origin.cube.core.module.Category
 import cn.origin.cube.core.module.Module
+import cn.origin.cube.core.settings.ModeSetting
 import cn.origin.cube.module.interfaces.ModuleInfo
+import cn.origin.cube.module.interfaces.Para
 import cn.origin.cube.utils.player.InventoryUtil
 import net.minecraft.client.gui.inventory.GuiContainer
 import net.minecraft.client.gui.inventory.GuiInventory
@@ -17,41 +19,53 @@ import net.minecraft.network.play.client.CPacketEntityAction
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent
 
+@Para(para = Para.ParaMode.Test)
 @ModuleInfo(name = "AutoTotem",
     descriptions = "A",
     category = Category.COMBAT)
 class AutoTote : Module() {
 
-    val stacked: BooleanSetting = registerSetting("Stacked", false)
-    val delay: FloatSetting = registerSetting("Delay",  100F, 10F, 300F).booleanVisible(stacked)
-    val delay2: FloatSetting = registerSetting("SecondDelay", 100F, 10F, 300F).booleanVisible(stacked)
-    val switchCount: FloatSetting = registerSetting("SwitchCount",  6F, 3F, 63F).booleanVisible(stacked)
-    val prepareCount: FloatSetting = registerSetting("PrepCount", 3F, 0F, 6F).booleanVisible(stacked)
-    val minCount: FloatSetting = registerSetting("MinCount", 23F, 1F, 63F).booleanVisible(stacked)
+    val mode: ModeSetting<ATMode> = registerSetting("Mode", ATMode.Normal)
+    val delay: FloatSetting = registerSetting("Delay",  100F, 10F, 300F).modeVisible(mode, ATMode.Stacked)
+    val delay2: FloatSetting = registerSetting("SecondDelay", 100F, 10F, 300F).modeVisible(mode, ATMode.Stacked)
+    val switchCount: FloatSetting = registerSetting("SwitchCount",  6F, 3F, 63F).modeVisible(mode, ATMode.Stacked)
+    val prepareCount: FloatSetting = registerSetting("PrepCount", 3F, 0F, 6F).modeVisible(mode, ATMode.Stacked)
+    val minCount: FloatSetting = registerSetting("MinCount", 23F, 1F, 63F).modeVisible(mode, ATMode.Stacked)
 
 
     @SubscribeEvent
     fun onTick(event: ClientTickEvent?) {
         if (fullNullCheck()) return
-        if (mc.currentScreen is GuiContainer && mc.currentScreen !is GuiInventory) return
-        val totemslot = InventoryUtil.getItemSlot(Items.TOTEM_OF_UNDYING)
-        if (mc.player.heldItemOffhand.item !== Items.TOTEM_OF_UNDYING && totemslot != -1) {
-            mc.playerController.windowClick(
-                mc.player.inventoryContainer.windowId,
-                totemslot,
-                0,
-                ClickType.PICKUP,
-                mc.player
-            )
-            mc.playerController.windowClick(mc.player.inventoryContainer.windowId, 45, 0, ClickType.PICKUP, mc.player)
-            mc.playerController.windowClick(
-                mc.player.inventoryContainer.windowId,
-                totemslot,
-                0,
-                ClickType.PICKUP,
-                mc.player
-            )
-            mc.playerController.updateController()
+        if(mode.value.equals(ATMode.Normal)) {
+            if (mc.currentScreen is GuiContainer && mc.currentScreen !is GuiInventory) return
+            val totemslot = InventoryUtil.getItemSlot(Items.TOTEM_OF_UNDYING)
+            if (mc.player.heldItemOffhand.item !== Items.TOTEM_OF_UNDYING && totemslot != -1) {
+                mc.playerController.windowClick(
+                    mc.player.inventoryContainer.windowId,
+                    totemslot,
+                    0,
+                    ClickType.PICKUP,
+                    mc.player
+                )
+                mc.playerController.windowClick(
+                    mc.player.inventoryContainer.windowId,
+                    45,
+                    0,
+                    ClickType.PICKUP,
+                    mc.player
+                )
+                mc.playerController.windowClick(
+                    mc.player.inventoryContainer.windowId,
+                    totemslot,
+                    0,
+                    ClickType.PICKUP,
+                    mc.player
+                )
+                mc.playerController.updateController()
+            }
+        }
+        if(mode.value.equals(ATMode.Stacked)){
+            StackedTotemUtil.doStack()
         }
         super.onUpdate()
     }
@@ -61,5 +75,9 @@ class AutoTote : Module() {
         if (event.getPacket<Packet<*>>() is CPacketClickWindow) {
             mc.player.connection.sendPacket(CPacketEntityAction(mc.player, CPacketEntityAction.Action.STOP_SPRINTING))
         }
+    }
+
+    enum class ATMode{
+        Stacked, Normal
     }
 }
