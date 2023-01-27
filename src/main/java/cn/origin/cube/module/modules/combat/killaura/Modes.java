9161377eb6202e69be3aed1d7c80b72c0390d7da
Canonball90 <nathanripley2017@gmail.com>
2023-01-27 09:11:11 -0800

    Changes

--- src/main/java/cn/origin/cube/module/modules/combat/killaura/Modes.java
+++ src/main/java/cn/origin/cube/module/modules/combat/killaura/Modes.java
@@ -2,15 +2,4 @@ package cn.origin.cube.module.modules.combat.killaura;
 
 public class Modes {
 
-    public static enum RenderMode{
-        Circle,Box
-    }
-
-    public static enum Mode{
-        Offhand,Main,Both
-    }
-
-    public static enum Page{
-        Target,AntiCheat,Render
-    }
 }
